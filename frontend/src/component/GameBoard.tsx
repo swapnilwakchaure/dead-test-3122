f81e408252678bf0d71e5dd028b2e0911040dc35
martina024 <107464455+martina024@users.noreply.github.com>
2023-03-04 17:21:20 +0530

    updated the code of board

--- frontend/src/component/GameBoard.tsx
+++ frontend/src/component/GameBoard.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import './GameBoard.css';
 import clickSound from '../assets/clickSound/clickSound.mp3';
 
-interface Props {
-  board: string[][];
-  handleColClick: (rowIndex: number, colIndex: number) => void;
-}
+interface Box {
+    image: string;
+    // add more properties here if needed
+  }
 
+interface Props {
+    board: Box[][];
+    handleColClick: (rowIndex: number, colIndex: number) => void;
+  }
+  
+  
 
 
 function GameBoard(props: Props) {
@@ -20,16 +26,16 @@ function GameBoard(props: Props) {
       {props.board.map((row, rowIndex) => (
         <div className="row" key={rowIndex}>
           {row.map((col, colIndex) => (
-            <div
-              className={`col ${col}`}
-              key={`${rowIndex}-${colIndex}`}
+            <div className={`col ${col.image}`} key={`${rowIndex}-${colIndex}`}
+
               onClick={(event) => {
                 props.handleColClick(rowIndex, colIndex);
                 playSound();
                 (event.currentTarget as HTMLDivElement).classList.add('clicked');
               }}
             >
-              <img src={col} alt="" className="img-thumbnail" />
+              <img src={col.image} alt="" className="img-thumbnail" />
+
             </div>
           ))}
         </div>
@@ -39,6 +45,75 @@ function GameBoard(props: Props) {
 }
 
 export default GameBoard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import './GameBoard.css';
+// import clickSound from '../assets/clickSound/clickSound.mp3';
+
+// interface Props {
+//   board: string[][];
+//   handleColClick: (rowIndex: number, colIndex: number) => void;
+// }
+
+
+
+// function GameBoard(props: Props) {
+//   const playSound = () => {
+//     const sound = new Audio(clickSound);
+//     sound.play();
+//   };
+
+//   return (
+//     <div className="game-board">
+//       {props.board.map((row, rowIndex) => (
+//         <div className="row" key={rowIndex}>
+//           {row.map((col, colIndex) => (
+//             <div
+//               className={`col ${col}`}
+//               key={`${rowIndex}-${colIndex}`}
+//               onClick={(event) => {
+//                 props.handleColClick(rowIndex, colIndex);
+//                 playSound();
+//                 (event.currentTarget as HTMLDivElement).classList.add('clicked');
+//               }}
+//             >
+//               <img src={col} alt="" className="img-thumbnail" />
+//             </div>
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default GameBoard;
 
 
 
